27df876abba0c13323793371c6d06a25c500675a
Nahom Tamirat <77017384+TNAHOM@users.noreply.github.com>
2023-12-07 14:26:41 +0300

    Added solution for Minimum Number of Operations to Move All Balls to Each Box

---
+++ camp/W1/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minOperations(self, boxes):
+        """
+        :type boxes: str
+        :rtype: List[int]
+        """
+        n = len(boxes)
+        ans = [0] * n
+        for i in range(n):
+            for j in range(n):
+                if boxes[j] == '1':
+                    ans[i] += abs(j-i)
+        return ans
