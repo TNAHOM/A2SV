50a0cb82f21f4aba61d4d69abdb7ce9acf8276d4
Nahom Tamirat <77017384+TNAHOM@users.noreply.github.com>
2024-01-12 17:41:47 +0300

    Added solution for Maximum Erasure Value

---
+++ camp/W7/maximum-erasure-value.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        dic = set()
+        output = 0
+        l = 0
+        r = 0
+        add = 0
+
+        while r < len(nums):
+            if nums[r] in dic:
+                add-=nums[l]
+                dic.remove(nums[l])
+                l+=1
+            else:
+                add+=nums[r]
+                dic.add(nums[r])
+                r+=1
+            output = max(add, output)
+            
+        return output
