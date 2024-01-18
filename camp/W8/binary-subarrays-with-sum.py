63208ce8c7cafa3949d2dea82c13fc913188bd36
Nahom Tamirat <77017384+TNAHOM@users.noreply.github.com>
2024-01-18 18:04:44 +0300

    Added solution for Binary Subarrays With Sum

---
+++ camp/W8/binary-subarrays-with-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], k: int) -> int:
+        prefix = 0
+        dic = {0:1}
+        count = 0
+
+        for x in nums:
+            prefix+=x
+
+            if prefix-k in dic:
+                count+=dic[prefix-k]
+            dic[prefix] = dic.get(prefix, 0)+1
+        
+        return count
