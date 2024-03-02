0ce4fcbba040a664745559e5c9b5109aab840301
Nahom Tamirat <77017384+TNAHOM@users.noreply.github.com>
2024-03-02 16:38:06 +0300

    Add solution for Design Circular Queue

---
+++ W8/leetcode/design-circular-queue.py
@@ -0,0 +1,47 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.que = deque()
+        self.size = k
+
+    def enQueue(self, value: int) -> bool:
+        if self.size > 0:
+            self.que.append(value)
+            self.size-=1
+            return True
+        return False
+
+    def deQueue(self) -> bool:
+        if self.que:
+            self.que.popleft()
+            self.size+=1
+            return True
+        return False
+    def Front(self) -> int:
+        if self.que: return self.que[0]
+        return -1
+
+    def Rear(self) -> int:
+        if self.que: return self.que[-1]
+        return -1
+
+    def isEmpty(self) -> bool:
+        if not self.que:
+            return True
+        return False
+
+    def isFull(self) -> bool:
+        if self.size == 0:
+            return True
+        return
+        
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
